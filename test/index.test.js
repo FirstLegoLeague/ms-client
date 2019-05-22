b6b4c125f6768e56f44bbc14368f1c1495418f03
Roy <2903801+2roy999@users.noreply.github.com>
2019-05-23 01:26:15 +0300

    Reformatting tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,132 +1,68 @@
-/* eslint promise/no-callback-in-promise: 0 */
 
 const chai = require('chai')
 const moxios = require('moxios')
+const Promise = require('bluebird')
 
 const { client } = require('../')
 
 const expect = chai.expect
 
+Promise.promisifyAll(moxios)
+
 const URL = 'http://url'
 const DATA = { param1: 'value1' }
+
+const DATA_METHODS = ['post', 'put']
 
 describe('ms-client', () => {
   beforeEach(() => moxios.install(client))
   afterEach(() => moxios.uninstall())
 
-  describe('get request', () => {
-    beforeEach(() => {
-      client.get(URL)
-    })
+  ;['get', 'post', 'put', 'delete'].forEach(testedMethod => {
+    describe(`${testedMethod.toUpperCase()} request`, () => {
+      if (DATA_METHODS.includes(testedMethod)) {
+        beforeEach(() => {
+          client[testedMethod](URL, DATA)
+        })
 
-    it('calls axios get', done => {
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        return request.respondWith({ status: 200 })
-          .then(() => {
-            expect(request.config.method).to.equals('get')
+        it('creates request with given data in JSON format', () => {
+          return moxios.waitAsync()
+            .then(() => moxios.requests.mostRecent())
+            .tap(request => request.respondWith({ status: 200 }))
+            .then(request => {
+              expect(request.config.data).to.equals(JSON.stringify(DATA))
+            })
+        })
+      } else {
+        beforeEach(() => {
+          client[testedMethod](URL)
+        })
+      }
+
+      it('creates request with correct method', () => {
+        return moxios.waitAsync()
+          .then(() => moxios.requests.mostRecent())
+          .tap(request => request.respondWith({ status: 200 }))
+          .then(request => {
+            expect(request.config.method).to.equals(testedMethod)
+          })
+      })
+
+      it('creates request with given URL', () => {
+        return moxios.waitAsync()
+          .then(() => moxios.requests.mostRecent())
+          .tap(request => request.respondWith({ status: 200 }))
+          .then(request => {
             expect(request.config.url).to.equals(URL)
-            done()
           })
       })
-    })
 
-    it('adds a correlation-id to the request', done => {
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        return request.respondWith({ status: 200 })
-          .then(() => {
+      it('adds a correlation-id to the request', () => {
+        return moxios.waitAsync()
+          .then(() => moxios.requests.mostRecent())
+          .tap(request => request.respondWith({ status: 200 }))
+          .then(request => {
             expect(Object.keys(request.config.headers)).to.include('correlation-id')
-            done()
-          })
-      })
-    })
-  })
-
-  describe('post request', () => {
-    beforeEach(() => {
-      client.post(URL, DATA)
-    })
-
-    it('calls axios post', done => {
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        return request.respondWith({ status: 200 })
-          .then(() => {
-            expect(request.config.method).to.equals('post')
-            expect(request.config.url).to.equals(URL)
-            expect(request.config.data).to.equals(JSON.stringify(DATA))
-            done()
-          })
-      })
-    })
-
-    it('adds a correlation-id to the request', done => {
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        return request.respondWith({ status: 200 })
-          .then(() => {
-            expect(Object.keys(request.config.headers)).to.include('correlation-id')
-            done()
-          })
-      })
-    })
-  })
-
-  describe('put request', () => {
-    beforeEach(() => {
-      client.put(URL, DATA)
-    })
-
-    it('calls axios put', done => {
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        return request.respondWith({ status: 200 })
-          .then(() => {
-            expect(request.config.method).to.equals('put')
-            expect(request.config.url).to.equals(URL)
-            expect(request.config.data).to.equals(JSON.stringify(DATA))
-            done()
-          })
-      })
-    })
-
-    it('adds a correlation-id to the request', done => {
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        return request.respondWith({ status: 200 })
-          .then(() => {
-            expect(Object.keys(request.config.headers)).to.include('correlation-id')
-            done()
-          })
-      })
-    })
-  })
-
-  describe('delete request', () => {
-    beforeEach(() => {
-      client.delete(URL, DATA)
-    })
-
-    it('calls axios delete', done => {
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        return request.respondWith({ status: 200 })
-          .then(() => {
-            expect(request.config.method).to.equals('delete')
-            expect(request.config.url).to.equals(URL)
-            done()
-          })
-      })
-    })
-
-    it('adds a correlation-id to the request', done => {
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        return request.respondWith({ status: 200 })
-          .then(() => {
-            expect(Object.keys(request.config.headers)).to.include('correlation-id')
-            done()
           })
       })
     })
