95fa75b87688f13447beb9354eed0dca26f4a98d
Idan Stark <idanstark42@gmail.com>
2019-10-21 00:42:15 +0300

    Fixed tests to work with detect-node which is load upon require

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,6 @@ const proxyquire = require('proxyquire')
 
 chai.use(spies)
 
-let isServer
 let correlationSpy
 let independenceSpy
 let clientIdentitySpy
@@ -14,40 +13,39 @@ let responsesLogSpy
 global.window = global.window || { }
 window.localStorage = global.localStorage
 
-const { createClient } = proxyquire('../', {
-  'detect-node': function () {
-    return isServer
-  },
-  './lib/correlation': {
-    correlate: function () {
-      correlationSpy.apply(this, arguments)
-    }
-  },
-  './lib/independence': {
-    makeIndependent: function () {
-      independenceSpy.apply(this, arguments)
-    }
-  },
-  './lib/client-identity': {
-    identifyClient: function () {
-      clientIdentitySpy.apply(this, arguments)
-    }
-  },
-  './lib/logging': {
-    logRequests: function () {
-      requestsLogSpy.apply(this, arguments)
+const reuquireClient = isServer => {
+  return proxyquire('../', {
+    'detect-node': isServer,
+    './lib/correlation': {
+      correlate: function () {
+        correlationSpy.apply(this, arguments)
+      }
     },
-    logResponses: function () {
-      responsesLogSpy.apply(this, arguments)
+    './lib/independence': {
+      makeIndependent: function () {
+        independenceSpy.apply(this, arguments)
+      }
+    },
+    './lib/client-identity': {
+      identifyClient: function () {
+        clientIdentitySpy.apply(this, arguments)
+      }
+    },
+    './lib/logging': {
+      logRequests: function () {
+        requestsLogSpy.apply(this, arguments)
+      },
+      logResponses: function () {
+        responsesLogSpy.apply(this, arguments)
+      }
     }
-  }
-})
+  })
+}
 
 const expect = chai.expect
 
 describe('ms-client', () => {
   beforeEach(() => {
-    isServer = false
     correlationSpy = chai.spy(() => {})
     independenceSpy = chai.spy(() => {})
     clientIdentitySpy = chai.spy(() => {})
@@ -56,61 +54,63 @@ describe('ms-client', () => {
   })
 
   it('returns a client which is not correlated if not server', () => {
-    isServer = false
+    const { createClient } = reuquireClient(false)
     createClient()
-    expect(correlationSpy).to.have.been.called()
+    expect(correlationSpy).to.not.have.been.called()
   })
 
   it('returns a client which is correlated if in server', () => {
-    isServer = true
+    const { createClient } = reuquireClient(true)
     createClient()
     expect(correlationSpy).to.have.been.called()
   })
 
   it('returns a client which does not log requests if not server', () => {
-    isServer = false
+    const { createClient } = reuquireClient(false)
     createClient()
-    expect(requestsLogSpy).to.have.been.called()
+    expect(requestsLogSpy).to.not.have.been.called()
   })
 
   it('returns a client which logs requests if in server', () => {
-    isServer = true
+    const { createClient } = reuquireClient(true)
     createClient()
     expect(requestsLogSpy).to.have.been.called()
   })
 
   it('returns a client which logs requests with passed options if in server and logging options passed', () => {
-    isServer = true
+    const { createClient } = reuquireClient(true)
     const loggingOptions = { }
     createClient({ logging: loggingOptions })
     expect(requestsLogSpy).to.have.been.called.with(loggingOptions)
   })
 
   it('returns a client which does not log responses if not server', () => {
-    isServer = false
+    const { createClient } = reuquireClient(false)
     createClient()
-    expect(responsesLogSpy).to.have.been.called()
+    expect(responsesLogSpy).to.not.have.been.called()
   })
 
   it('returns a client which logs responses if in server', () => {
-    isServer = true
+    const { createClient } = reuquireClient(true)
     createClient()
     expect(responsesLogSpy).to.have.been.called()
   })
 
   it('returns a client which logs responses with passed options if in server and logging options passed', () => {
-    isServer = true
+    const { createClient } = reuquireClient(true)
     const loggingOptions = { }
     createClient({ logging: loggingOptions })
     expect(responsesLogSpy).to.have.been.called.with(loggingOptions)
   })
 
   it('returns a client which is not defaulty independent', () => {
+    const { createClient } = reuquireClient(true)
     createClient()
     expect(independenceSpy).to.not.have.been.called()
   })
 
   it('returns a client which is independent if requested to be', () => {
+    const { createClient } = reuquireClient(true)
     createClient({ independent: true })
     expect(independenceSpy).to.have.been.called()
   })
